423116b4e05a54baae4f7872834c9afa0b5bd3ff
Darlin Ruben Nina C. <chinausto@gmail.com>
2023-11-02 15:27:30 -0400

    5

--- config/settings.py
+++ config/settings.py
@@ -14,10 +14,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = [ '*' ]
 
 CORS_ALLOWED_ORIGINS = [
-    
+    '*'
 ]
 
 # Application definition
